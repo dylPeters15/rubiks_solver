0cdd82642b848141248e8d8007d7d2d911e157ee
Dylan Peters <dylanlpeters@gmail.com>
2018-12-15 23:00:04 -0600

    Ready to start modifying A Star search

--- ThreeByThreeModel.py
+++ ThreeByThreeModel.py
@@ -1,68 +1,43 @@
 class ThreeByThreeModel:
+    solved = "ooooooooogggwwwbbbgggwwwbbbgggwwwbbbrrrrrrrrryyyyyyyyy"
 
     def __init__(self, initial_config=None):
-        white = "white"
-        red = "red"
-        blue = "blue"
-        orange = "orange"
-        green = "green"
-        yellow = "yellow"
         if initial_config == None:
-            self.squares = [
-                [[white, white, white], [white, white, white], [white, white, white]],
-                [[red, red, red], [red, red, red], [red, red, red]],
-                [[blue, blue, blue], [blue, blue, blue], [blue, blue, blue]],
-                [[orange, orange, orange], [orange, orange, orange], [orange, orange, orange]],
-                [[green, green, green], [green, green, green], [green, green, green]],
-                [[yellow, yellow, yellow], [yellow, yellow, yellow], [yellow, yellow, yellow]]
-            ]
+            self.data = ThreeByThreeModel.solved
         else:
-            squares = initial_config
+            self.data = initial_config
+
+        self.distance_from_solved = 0
+        for i in range(len(self.data)):
+            if self.data[i] != ThreeByThreeModel.solved[i]:
+                self.distance_from_solved += 1
+
+    def get_all_next_moves(self):
+        #TODO
+        return
 
     #simplified instruction set:
 
-    def left_down(self):
+    def _left_down(self):
         #TODO
         return
 
-    def right_down(self):
+    def _right_down(self):
         #TODO
         return
 
-    def top_right(self):
+    def _top_right(self):
         #TODO
         return
 
-    def bottom_right(self):
+    def _bottom_right(self):
         #TODO
         return
 
-    def front_clockwise(self):
+    def _front_clockwise(self):
         #TODO
         return
 
-    def back_clockwise(self):
+    def _back_clockwise(self):
         #TODO
         return
-
-    def distance_from_solved(self):
-        return
-
-    def is_valid(self):
-        return
-
-    def value(self):
-        value = []
-        for face in self.squares:
-            for row in face:
-                for square in row:
-                    value.append(square)
-        return value
-
-    def string_value(self):
-        value = ""
-        for face in self.squares:
-            for row in face:
-                for square in row:
-                    value += square
-        return value
